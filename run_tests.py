b389a4ae0abf1bac0d7c2a840e297947a6883681
Omersal150 <omer.salamander@gmail.com>
2024-06-16 20:35:25 +0300

    Final changes

--- run_tests.py
+++ run_tests.py
@@ -2,7 +2,7 @@ import pytest
 
 def run_tests():
     # Run pytest from the current directory, specifying the root directory where tests are located
-    pytest.main(['--rootdir=root/Persudoku/app/tests'])
+    pytest.main()
 
 if __name__ == "__main__":
     run_tests()
